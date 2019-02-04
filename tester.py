e6f06440e4f96aa403b75c963e37f0885f1aa829
willysantoso05 <willysantoso05@gmail.com>
2019-02-04 18:25:12 +0700

    First_Tester

--- tester.py
+++ tester.py
@@ -9,7 +9,7 @@ candidates = [brute,greedy1, greedy2]
 loop = 10000
 
 def get4Number():
-    return list(itertools.combinations_with_replacement([1,2,3,4,5,6,7,8,9,10,11,12,13],4))
+    return list(itertools.permutations([1,2,3,4,5,6,7,8,9,10,11,12,13],4))[random.randint(0,17159)]
 # [random.randint(0,17159)]
 if __name__ == "__main__":
     if len(sys.argv)>1:
