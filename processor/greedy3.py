ec49f5edc717443900ff749d2319a43fab4e2b2a
jrandiny <jrandiny2@gmail.com>
2019-02-10 21:46:16 +0700

    Add candidate greedy algorithm

---
+++ processor/greedy3.py
@@ -0,0 +1,33 @@
+import utils
+
+def getName():
+    return "Josh () Greedy"
+
+def calculate(param):
+    input = list(param)
+
+    input.sort(reverse=True)
+    
+    # result = input[0]
+    tempExpr = str(input[0]) 
+    maxExpr = tempExpr
+
+    for i in range(1,4):
+        tempMax = "empty"
+        for enclosed in [False,True]:
+            for opr in ['+','-','*','/']:
+                tempExpr = maxExpr
+                if(enclosed):
+                    tempExpr = "(" + tempExpr + ")"
+                tempExpr = tempExpr+opr
+                tempExpr = tempExpr+str(input[i])
+                tempScore = utils.countScore(tempExpr)
+
+                if(tempMax == "empty"):
+                    tempMax = tempExpr
+
+                if(tempScore>=utils.countScore(tempMax)):
+                    tempMax = tempExpr
+        maxExpr = tempMax
+
+    return [utils.countScore(maxExpr),maxExpr]
