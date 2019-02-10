40f5ad1c1ca78137e74cb0fad92056327e68c6f3
jrandiny <jrandiny2@gmail.com>
2019-02-10 21:46:44 +0700

    Update brute processr to try bracket

--- processor/brute.py
+++ processor/brute.py
@@ -25,13 +25,24 @@ def calculate(param):
 
     for num in number_permut:
         for sign in sign_permut:
-            expr = "{:.0f}{}{:.0f}{}{:.0f}{}{:.0f}".format(num[0],getSymbol(sign[0]),num[1],getSymbol(sign[1]),num[2],getSymbol(sign[2]),num[3])
-            listexpr.append(expr)
-            listskor.append(utils.countScore(expr))
+            for enclosing in [1,2,3,4,5]:
+                if enclosing == 1: 
+                    expr = "({:.0f}{}{:.0f}){}({:.0f}{}{:.0f})".format(num[0],getSymbol(sign[0]),num[1],getSymbol(sign[1]),num[2],getSymbol(sign[2]),num[3])
+                elif enclosing == 2:
+                    expr = "(({:.0f}{}{:.0f}){}{:.0f}){}{:.0f}".format(num[0],getSymbol(sign[0]),num[1],getSymbol(sign[1]),num[2],getSymbol(sign[2]),num[3])
+                elif enclosing == 3:
+                    expr = "{:.0f}{}(({:.0f}{}{:.0f}){}{:.0f})".format(num[0],getSymbol(sign[0]),num[1],getSymbol(sign[1]),num[2],getSymbol(sign[2]),num[3])
+                elif enclosing == 4:
+                    expr = "({:.0f}{}({:.0f}{}{:.0f})){}{:.0f}".format(num[0],getSymbol(sign[0]),num[1],getSymbol(sign[1]),num[2],getSymbol(sign[2]),num[3])
+                elif enclosing == 5:
+                    expr = "{:.0f}{}({:.0f}{}({:.0f}{}{:.0f}))".format(num[0],getSymbol(sign[0]),num[1],getSymbol(sign[1]),num[2],getSymbol(sign[2]),num[3])
+            
+                listexpr.append(expr)
+                listskor.append(utils.countScore(expr))
             
     maxidx = 0
     for i in range (len(listskor)):
-      if (listskor[i]>listskor[maxidx]):
-        maxidx = i
+        if (listskor[i]>listskor[maxidx]):
+            maxidx = i
 
     return [listskor[maxidx],listexpr[maxidx]]
