3bd6fc23d28b1b8c7bde09eb74cfc4d32d1723d7
jrandiny <jrandiny2@gmail.com>
2019-02-12 23:48:49 +0700

    Update greedy algorithm

--- processor/greedy5.py
+++ processor/greedy5.py
@@ -13,43 +13,71 @@ def calculate(param):
     input = list(param)
 
     input.sort(reverse = True)
-    
-    # result = input[0]
+
     tempExpr = [str(input[0])]
     maxExpr = tempExpr
 
     for i in range(1,4):
-        tempMax = "empty"
-        for pos in[0,1]:
-            for opr in ['+','-','*','/']:
-                tempExpr = maxExpr
-                
-                if(pos==0):
-                    tempExpr = tempExpr+[opr]
-                    tempExpr = tempExpr+[str(input[i])]
-                else:
-                    tempExpr = [opr]+tempExpr
-                    tempExpr = [str(input[i])]+tempExpr
+        tempMax = []
+        for opr in ['+','-','*','/']:
+            index = 0
+            panjang = len(maxExpr) -1
+            while(index<=panjang):
+                tempExpr = maxExpr.copy()
+                tempExpr.insert(index,opr)
+                tempExpr.insert(index,str(input[i]))
+                index = index + 2
                 tempScore = utils.countScore(toText(tempExpr))
 
-                if(tempMax == "empty"):
+                if(tempMax == []):
                     tempMax = tempExpr
 
                 if(tempScore>=utils.countScore(toText(tempMax))):
                     tempMax = tempExpr
-        maxExpr = tempMax
+                    
+            index = 1
+            panjang = len(maxExpr)
+            while(index<=panjang):
+                tempExpr = maxExpr.copy()
+                tempExpr.insert(index,str(input[i]))
+                tempExpr.insert(index,opr)
+
+                index = index + 2
+                tempScore = utils.countScore(toText(tempExpr))
+
+                if(tempMax == []):
+                    tempMax = tempExpr
+                
+                if(tempScore>=utils.countScore(toText(tempMax))):
+                    tempMax = tempExpr
+                
+        maxExpr = tempMax.copy()
+
         if(i==3):
-            for enclosing in [1,2]:
-                tempExpr = maxExpr
+            tempMax = maxExpr.copy()
+            for enclosing in [1,2,3,4]:
+                tempExpr = maxExpr.copy()
                 if(enclosing==1):
-                    tempExpr = ['((']+tempExpr
+                    tempExpr = ['((']+tempExpr.copy()
                     tempExpr.insert(4,')')
                     tempExpr.insert(7,')')
                 elif(enclosing ==2):
-                    pass
-                if(utils.countScore(tempExpr)>=utils.countScore(toText(maxExpr))):
-                    maxExpr = tempExpr
+                    tempExpr = ['('] + tempExpr.copy()
+                    tempExpr.insert(4,')')
+                    tempExpr.insert(6,'(')
+                    tempExpr = tempExpr.copy() + [')']
+                elif(enclosing ==3):
+                    tempExpr.insert(2,'((')
+                    tempExpr.insert(6,')')
+                    tempExpr = tempExpr.copy() + [')']
+                elif(enclosing ==4):
+                    tempExpr.insert(2,'(')
+                    tempExpr.insert(6,')')
+
+                if(utils.countScore(toText(tempExpr))>=utils.countScore(toText(tempMax))):
+                    tempMax = tempExpr.copy()
+            maxExpr = tempMax.copy()
 
 
 
-    return [utils.countScore(maxExpr),maxExpr]
+    return [utils.countScore(toText(maxExpr)),toText(maxExpr)]
